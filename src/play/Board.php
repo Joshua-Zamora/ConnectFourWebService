32f352ffe6bd89fa232d24974640a1184b3d52db
Joshua Zamora <54697058+ZanteZyaire@users.noreply.github.com>
2020-09-29 12:02:01 -0600

    Game Won Method
    Implemented a method to check if the given player has a winning row

--- src/play/Board.php
+++ src/play/Board.php
@@ -36,5 +36,43 @@ class Board
         return true;
     }
 
+    function checkForWinningRow($player) {
+        for ($i = 0; $i < 5; $i++) {     // Checks Horizontal groups
+            for ($j = 0; $j < 3; $j++) {
+                if ($this->board[$i][$j    ] == $player &&
+                    $this->board[$i][$j + 1] == $player &&
+                    $this->board[$i][$j + 2] == $player &&
+                    $this->board[$i][$j + 3] == $player) return true;
+            }
+        }
 
+        for ($i = 0; $i < 2; $i++) {     // Checks vertical groups
+            for ($j = 0; $j < 6; $j++) {
+                if ($this->board[$i    ][$j] == $player &&
+                    $this->board[$i + 1][$j] == $player &&
+                    $this->board[$i + 2][$j] == $player &&
+                    $this->board[$i + 3][$j] == $player) return true;
+            }
+        }
+
+        for ($i = 3; $i < 5; $i++) {     // Checks Ascending diagonal groups
+            for ($j = 0; $j < 3; $j++) {
+                if ($this->board[$i    ][$j    ] == $player &&
+                    $this->board[$i - 1][$j + 1] == $player &&
+                    $this->board[$i - 2][$j + 2] == $player &&
+                    $this->board[$i - 3][$j + 3] == $player) return true;
+            }
+        }
+
+        for ($i = 3; $i < 5; $i++) {     // Checks Descending Diagonal groups
+            for ($j = 3; $j < 6; $j++) {
+                if ($this->board[$i    ][$j    ] == $player &&
+                    $this->board[$i - 1][$j - 1] == $player &&
+                    $this->board[$i - 2][$j - 2] == $player &&
+                    $this->board[$i - 3][$j - 3] == $player) return true;
+            }
+        }
+
+        return false;
+    }
 }
