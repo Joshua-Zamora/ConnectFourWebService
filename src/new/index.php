dfdeec0c0d10a3bc33d5725fabbefe1d848397f0
Joshua Zamora <54697058+ZanteZyaire@users.noreply.github.com>
2020-09-30 14:10:02 -0600

    Fixed bug issue that resulted in incorrect echoed responses

--- src/new/index.php
+++ src/new/index.php
@@ -1,33 +1,36 @@
 <?php
+require dirname(dirname(__FILE__)) . "/play/Board.php";
+
 define('WRITE', dirname(dirname(__FILE__))."/writable/");
-define('STRATEGY', "strategy");
+define('STRATEGY', $_GET["strategy"]);
 
 main();
 
 function main() {
-    $strategies = array("Smart"=> 0, "Random"=> 1); // supported strategies
-    $strategy = $_GET[STRATEGY];
+    $strategies = array("Smart"=> 0, "Random"=> 1, "smart" => 2, "random" => 3); // supported strategies
     $info = array();
 
-    if (!array_key_exists(STRATEGY, $_GET) || $_GET[STRATEGY] == "") {
+    if (!array_key_exists(STRATEGY, $strategies)) {
         $info['response'] = false;
         $info['reason'] = "Strategy not specified";
-    } elseif (in_array(strtolower($strategy), array_map('strtolower', $strategies))) {
-        $info['response'] = true;
-        $info['pid'] = uniqid();
-    } else {
+    }
+    elseif (STRATEGY == "") {
         $info['response'] = false;
         $info['reason'] = "Strategy unknown";
     }
+   else {
+       $info['response'] = true;
+       $board = new Board();
+
+       if (STRATEGY == "Random" || STRATEGY == "random") {
+           $info['pid'] = 'R' . uniqid();
+           file_put_contents(WRITE . $info['pid'] . 'txt', json_encode($board->board));
+       }
+       else {
+           $info['pid'] = 'S' . uniqid();
+           file_put_contents(WRITE . $info['pid'] . 'txt', json_encode($board->board));
+       }
+   }
+
     echo json_encode($info);
-
-    if ($info['response'] == true) {
-
-        $board = new Board();
-
-        if ($strategy == "Random" || $strategy == "random")
-            file_put_contents(WRITE . 'R' . $info['pid'] . 'txt', json_encode($board->board));
-        else
-            file_put_contents(WRITE . 'S' . $info['pid'] . 'txt', json_encode($board->board));
-    }
 }
