7f84057f606d90cbd4631815d58eb6ce49c39555
Joshua Zamora <54697058+ZanteZyaire@users.noreply.github.com>
2020-09-30 15:12:18 -0600

    Fixed error called index undefined

--- src/new/index.php
+++ src/new/index.php
@@ -2,7 +2,7 @@
 require dirname(dirname(__FILE__)) . "/play/Board.php";
 
 define('WRITE', dirname(dirname(__FILE__))."/writable/");
-define('STRATEGY', $_GET["strategy"]);
+define('STRATEGY', "strategy");
 
 main();
 
@@ -10,19 +10,19 @@ function main() {
     $strategies = array("Smart"=> 0, "Random"=> 1, "smart" => 2, "random" => 3); // supported strategies
     $info = array();
 
-    if (!array_key_exists(STRATEGY, $strategies)) {
-        $info['response'] = false;
-        $info['reason'] = "Strategy unknown";
-    }
-    elseif (STRATEGY == "") {
+    if (!isset($_GET[STRATEGY])) {
         $info['response'] = false;
         $info['reason'] = "Strategy not specified";
     }
-   else {
+    elseif (!array_key_exists($_GET[STRATEGY], $strategies)) {
+        $info['response'] = false;
+        $info['reason'] = "Strategy unknown";
+    }
+    else {
        $info['response'] = true;
        $board = new Board();
 
-       if (STRATEGY == "Random" || STRATEGY == "random") {
+       if ($_GET[STRATEGY] == "Random" ||$_GET[STRATEGY] == "random") {
            $info['pid'] = 'R' . uniqid();
            file_put_contents(WRITE . $info['pid'] . 'txt', json_encode($board->board));
        }
