957c8e12665588e7c023b632efe0ef386f787d07
Joshua Zamora <54697058+ZanteZyaire@users.noreply.github.com>
2020-10-01 11:40:58 -0600

    Removed infinite loop bug

--- src/play/Random.php
+++ src/play/Random.php
@@ -1,12 +1,14 @@
 <?php
 
+require_once "Strategy.php";
+
 class Random extends Strategy {
     function GetComputedCoordinates($board) {
 
-        do {
-            $xCoordinate = rand(0, 6);
-        } while(!$board->isValidCoordinate($xCoordinate));
+        for ($xCoordinate = 0; $xCoordinate < 7; $xCoordinate++) {
+            if ($board->board[0][$xCoordinate] == 0) return $xCoordinate;
+        }
 
-        return $xCoordinate;
+        return 0;
     }
 }
