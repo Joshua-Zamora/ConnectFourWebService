afa7ba8b197698b8204cb8cdbf864946cf9cb7e0
Joshua Zamora <54697058+ZanteZyaire@users.noreply.github.com>
2020-10-01 11:42:09 -0600

    Increased index values to match a ordinary connect 4 board

--- src/play/Board.php
+++ src/play/Board.php
@@ -1,9 +1,7 @@
 <?php
 
-
 class Board
 {
-
     public $board;
 
     function __construct() {
@@ -17,28 +15,25 @@ class Board
         );
     }
 
-    function isValidCoordinate($xCoordinate) {
-        if ($xCoordinate < 0 || $this->board[0][$xCoordinate] != 0) return false;
-        else return true;
-    }
-
     function insertDisc($xCoordinate, $player) {
         for ($yCoordinate = 5; $yCoordinate > -1; $yCoordinate--) {
-            if ($this->board[$yCoordinate][$xCoordinate] == 0)
+            if ($this->board[$yCoordinate][$xCoordinate] == 0) {
                 $this->board[$yCoordinate][$xCoordinate] = $player;
+                return;
+            }
         }
     }
 
     function boardIsFull() {
-        foreach ($this->board[0] as $slot)
-            if ($slot == 0) return false;
+        for ($xCoordinate = 0; $xCoordinate < 7; $xCoordinate++)
+            if ($this->board[0][$xCoordinate] == 0) return false;
 
         return true;
     }
 
     function checkForWinningRow($player) {
         for ($i = 0; $i < 6; $i++) {     // Checks Horizontal groups
-            for ($j = 0; $j < 3; $j++) {
+            for ($j = 0; $j < 4; $j++) {
                 if ($this->board[$i][$j    ] == $player &&
                     $this->board[$i][$j + 1] == $player &&
                     $this->board[$i][$j + 2] == $player &&
@@ -46,8 +41,8 @@ class Board
             }
         }
 
-        for ($i = 0; $i < 2; $i++) {     // Checks vertical groups
-            for ($j = 0; $j < 6; $j++) {
+        for ($i = 0; $i < 3; $i++) {     // Checks vertical groups
+            for ($j = 0; $j < 7; $j++) {
                 if ($this->board[$i    ][$j] == $player &&
                     $this->board[$i + 1][$j] == $player &&
                     $this->board[$i + 2][$j] == $player &&
@@ -56,7 +51,7 @@ class Board
         }
 
         for ($i = 3; $i < 6; $i++) {     // Checks Ascending diagonal groups
-            for ($j = 0; $j < 3; $j++) {
+            for ($j = 0; $j < 4; $j++) {
                 if ($this->board[$i    ][$j    ] == $player &&
                     $this->board[$i - 1][$j + 1] == $player &&
                     $this->board[$i - 2][$j + 2] == $player &&
@@ -64,8 +59,8 @@ class Board
             }
         }
 
-        for ($i = 3; $i < 5; $i++) {     // Checks Descending Diagonal groups
-            for ($j = 3; $j < 6; $j++) {
+        for ($i = 3; $i < 6; $i++) {     // Checks Descending Diagonal groups
+            for ($j = 3; $j < 7; $j++) {
                 if ($this->board[$i    ][$j    ] == $player &&
                     $this->board[$i - 1][$j - 1] == $player &&
                     $this->board[$i - 2][$j - 2] == $player &&
