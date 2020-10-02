9e6901c2b2eab3a56f966b1ecbb0680f0514cd35
Joshua Zamora <54697058+ZanteZyaire@users.noreply.github.com>
2020-10-01 22:47:35 -0600

    Made a simple AI

--- src/play/Smart.php
+++ src/play/Smart.php
@@ -2,77 +2,169 @@
 
 require_once "Strategy.php";
 
-class Smart extends Strategy {
-    function GetComputedCoordinates($board) {
-        for ($i = 0; $i < 5; $i++) {     // Checks Horizontal groups
-            for ($j = 0; $j < 3; $j++) {
-                if ($board->board[$i][$j + 1] == 2 &&
+class Smart extends Strategy
+{
+    function GetComputedCoordinates($board)
+    {
+        for ($i = 0; $i < 6; $i++) {     // Checks Attacking Horizontal groups
+            for ($j = 0; $j < 4; $j++) {
+                if ($board->board[$i][$j    ] == 0 &&
+                    $board->board[$i][$j + 1] == 2 &&
                     $board->board[$i][$j + 2] == 2 &&
                     $board->board[$i][$j + 3] == 2) return $j;
 
                 elseif ($board->board[$i][$j    ] == 2 &&
+                        $board->board[$i][$j + 1] == 0 &&
+                        $board->board[$i][$j + 2] == 2 &&
+                        $board->board[$i][$j + 3] == 2) return ($j + 1);
+
+                elseif ($board->board[$i][$j    ] == 2 &&
                         $board->board[$i][$j + 1] == 2 &&
-                        $board->board[$i][$j + 2] == 2) return ($j + 3);
+                        $board->board[$i][$j + 2] == 0 &&
+                        $board->board[$i][$j + 3] == 2) return ($j + 2);
 
-                elseif ($board->board[$i][$j + 1] == 1 &&
-                        $board->board[$i][$j + 2] == 1 &&
-                        $board->board[$i][$j + 3] == 1) return $j;
-
-                elseif ($board->board[$i][$j    ] == 1 &&
-                        $board->board[$i][$j + 1] == 1 &&
-                        $board->board[$i][$j + 2] == 1) return ($j + 3);
+                elseif ($board->board[$i][$j    ] == 2 &&
+                        $board->board[$i][$j + 1] == 2 &&
+                        $board->board[$i][$j + 2] == 2 &&
+                        $board->board[$i][$j + 3] == 0) return ($j + 3);
             }
         }
 
-        for ($i = 0; $i < 2; $i++) {     // Checks vertical groups
-            for ($j = 0; $j < 6; $j++) {
-                if ($board->board[$i    ][$j] == 2 &&
+        for ($i = 0; $i < 3; $i++) {     // Checks Attacking vertical groups
+            for ($j = 0; $j < 7; $j++) {
+                if ($board->board[$i    ][$j] == 0 &&
                     $board->board[$i + 1][$j] == 2 &&
-                    $board->board[$i + 2][$j] == 2) return $j;
-
-                elseif ($board->board[$i + 1][$j] == 1 &&
-                        $board->board[$i + 2][$j] == 1 &&
-                        $board->board[$i + 3][$j] == 1) return $j;
+                    $board->board[$i + 2][$j] == 2 &&
+                    $board->board[$i + 3][$j] == 2) return $j;
             }
         }
 
-        for ($i = 3; $i < 5; $i++) {     // Checks Ascending diagonal groups
-            for ($j = 0; $j < 3; $j++) {
-                if ($board->board[$i - 1][$j + 1] == 2 &&
+        for ($i = 3; $i < 6; $i++) {     // Checks Attacking Ascending diagonal groups
+            for ($j = 0; $j < 4; $j++) {
+                if ($board->board[$i    ][$j    ] == 0 &&
+                    $board->board[$i - 1][$j + 1] == 2 &&
                     $board->board[$i - 2][$j + 2] == 2 &&
                     $board->board[$i - 3][$j + 3] == 2) return $j;
 
                 elseif ($board->board[$i    ][$j    ] == 2 &&
+                        $board->board[$i - 1][$j + 1] == 0 &&
+                        $board->board[$i - 2][$j + 2] == 2 &&
+                        $board->board[$i - 3][$j + 3] == 2) return ($j + 1);
+
+                elseif ($board->board[$i    ][$j    ] == 2 &&
                         $board->board[$i - 1][$j + 1] == 2 &&
-                        $board->board[$i - 2][$j + 2] == 2) return ($j + 3);
+                        $board->board[$i - 2][$j + 2] == 0 &&
+                        $board->board[$i - 3][$j + 3] == 2) return ($j + 2);
 
-                elseif ($board->board[$i - 1][$j + 1] == 1 &&
-                        $board->board[$i - 2][$j + 2] == 1 &&
-                        $board->board[$i - 3][$j + 3] == 1) return $j;
-
-                elseif ($board->board[$i    ][$j    ] == 1 &&
-                        $board->board[$i - 1][$j + 1] == 1 &&
-                        $board->board[$i - 2][$j + 2] == 1) return ($j + 3);
+                elseif ($board->board[$i    ][$j    ] == 2 &&
+                        $board->board[$i - 1][$j + 1] == 2 &&
+                        $board->board[$i - 2][$j + 2] == 2 &&
+                        $board->board[$i - 3][$j + 3] == 0) return ($j + 3);
             }
         }
 
-        for ($i = 3; $i < 5; $i++) {     // Checks Descending Diagonal groups
-            for ($j = 3; $j < 6; $j++) {
-                if ($board->board[$i - 1][$j - 1] == 2 &&
+        for ($i = 3; $i < 6; $i++) {     // Checks Attacking Descending Diagonal groups
+            for ($j = 3; $j < 7; $j++) {
+                if ($board->board[$i    ][$j    ] == 0 &&
+                    $board->board[$i - 1][$j - 1] == 2 &&
                     $board->board[$i - 2][$j - 2] == 2 &&
                     $board->board[$i - 3][$j - 3] == 2) return $j;
 
                 elseif ($board->board[$i    ][$j    ] == 2 &&
-                        $board->board[$i - 1][$j - 1] == 2 &&
-                        $board->board[$i - 2][$j - 2] == 2 ) return ($j - 3);
+                        $board->board[$i - 1][$j - 1] == 0 &&
+                        $board->board[$i - 2][$j - 2] == 2 &&
+                        $board->board[$i - 3][$j - 3] == 2) return ($j - 1);
 
-                elseif ($board->board[$i - 1][$j - 1] == 1 &&
+                elseif ($board->board[$i    ][$j    ] == 2 &&
+                        $board->board[$i - 1][$j - 1] == 2 &&
+                        $board->board[$i - 2][$j - 2] == 0 &&
+                        $board->board[$i - 3][$j - 3] == 2) return ($j - 2);
+
+                elseif ($board->board[$i    ][$j    ] == 2 &&
+                        $board->board[$i - 1][$j - 1] == 2 &&
+                        $board->board[$i - 2][$j - 2] == 2 &&
+                        $board->board[$i - 3][$j - 3] == 0) return ($j - 3);
+            }
+        }
+
+        for ($i = 0; $i < 6; $i++) {     // Checks Defending Horizontal groups
+            for ($j = 0; $j < 4; $j++) {
+                if ($board->board[$i][$j    ] == 0 &&
+                    $board->board[$i][$j + 1] == 1 &&
+                    $board->board[$i][$j + 2] == 1 &&
+                    $board->board[$i][$j + 3] == 1) return $j;
+
+                elseif ($board->board[$i][$j    ] == 1 &&
+                        $board->board[$i][$j + 1] == 0 &&
+                        $board->board[$i][$j + 2] == 1 &&
+                        $board->board[$i][$j + 3] == 1) return ($j + 1);
+
+                elseif ($board->board[$i][$j    ] == 1 &&
+                        $board->board[$i][$j + 1] == 1 &&
+                        $board->board[$i][$j + 2] == 0 &&
+                        $board->board[$i][$j + 3] == 1) return ($j + 2);
+
+                elseif ($board->board[$i][$j    ] == 1 &&
+                        $board->board[$i][$j + 1] == 1 &&
+                        $board->board[$i][$j + 2] == 1 &&
+                        $board->board[$i][$j + 3] == 0) return ($j + 3);
+            }
+        }
+
+        for ($i = 0; $i < 3; $i++) {     // Checks Defending vertical groups
+            for ($j = 0; $j < 7; $j++) {
+                if ($board->board[$i    ][$j] == 0 &&
+                    $board->board[$i + 1][$j] == 1 &&
+                    $board->board[$i + 2][$j] == 1 &&
+                    $board->board[$i + 3][$j] == 1) return $j;
+            }
+        }
+
+        for ($i = 3; $i < 6; $i++) {     // Checks Defending Ascending diagonal groups
+            for ($j = 0; $j < 4; $j++) {
+                if ($board->board[$i    ][$j    ] == 0 &&
+                    $board->board[$i - 1][$j + 1] == 1 &&
+                    $board->board[$i - 2][$j + 2] == 1 &&
+                    $board->board[$i - 3][$j + 3] == 1) return $j;
+
+                elseif ($board->board[$i    ][$j    ] == 1 &&
+                        $board->board[$i - 1][$j + 1] == 0 &&
+                        $board->board[$i - 2][$j + 2] == 1 &&
+                        $board->board[$i - 3][$j + 3] == 1) return ($j + 1);
+
+                elseif ($board->board[$i    ][$j    ] == 1 &&
+                        $board->board[$i - 1][$j + 1] == 1 &&
+                        $board->board[$i - 2][$j + 2] == 0 &&
+                        $board->board[$i - 3][$j + 3] == 1) return ($j + 2);
+
+                elseif ($board->board[$i    ][$j    ] == 1 &&
+                        $board->board[$i - 1][$j + 1] == 1 &&
+                        $board->board[$i - 2][$j + 2] == 1 &&
+                        $board->board[$i - 3][$j + 3] == 0) return ($j + 3);
+            }
+        }
+
+        for ($i = 3; $i < 6; $i++) {     // Checks Defending Descending Diagonal groups
+            for ($j = 3; $j < 7; $j++) {
+                if ($board->board[$i    ][$j    ] == 0 &&
+                    $board->board[$i - 1][$j - 1] == 1 &&
+                    $board->board[$i - 2][$j - 2] == 1 &&
+                    $board->board[$i - 3][$j - 3] == 1) return $j;
+
+                elseif ($board->board[$i    ][$j    ] == 1 &&
+                        $board->board[$i - 1][$j - 1] == 0 &&
                         $board->board[$i - 2][$j - 2] == 1 &&
-                        $board->board[$i - 3][$j - 3] == 1) return $j;
+                        $board->board[$i - 3][$j - 3] == 1) return ($j - 1);
 
                 elseif ($board->board[$i    ][$j    ] == 1 &&
                         $board->board[$i - 1][$j - 1] == 1 &&
-                        $board->board[$i - 2][$j - 2] == 1 ) return ($j - 3);
+                        $board->board[$i - 2][$j - 2] == 0 &&
+                        $board->board[$i - 3][$j - 3] == 1) return ($j - 2);
+
+                elseif ($board->board[$i    ][$j    ] == 1 &&
+                        $board->board[$i - 1][$j - 1] == 1 &&
+                        $board->board[$i - 2][$j - 2] == 1 &&
+                        $board->board[$i - 3][$j - 3] == 0) return ($j - 3);
             }
         }
 
